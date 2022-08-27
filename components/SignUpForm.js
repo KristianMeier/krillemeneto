78e4938c49b19a1dc73fae0cc7c372e3740edbfd
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-08-27 18:42:54 +0200

    all komponents refactored.

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -5,51 +5,44 @@ import { Heading } from './Heading'
 
 export const SignUpForm = ({ settings }) => {
   return (
-    <div className='px-4'>
-      <form
-        action='/api/sign-up'
-        method='post'
-        className='grid w-full max-w-xl grid-cols-1 gap-6'
-      >
+    <div className='signupform-padding'>
+      <form action='/api/sign-up' method='post' className='signupform-grid'>
         {prismicH.isFilled.richText(settings.data.newsletterDisclaimer) && (
-          <div className='text-center font-serif tracking-tight text-slate-500'>
+          <div className='signupform-text'>
             <PrismicRichText
               field={settings.data.newsletterDescription}
               components={{
                 heading1: ({ children }) => (
-                  <Heading as='h2' className='mb-4 last:mb-0'>
+                  <Heading as='h2' className='signupform-heading'>
                     {children}
                   </Heading>
                 ),
                 paragraph: ({ children }) => (
-                  <p className='mb-4 italic last:mb-0'>{children}</p>
+                  <p className='signupform-headingtwo'>{children}</p>
                 ),
               }}
             />
           </div>
         )}
-        <div className='grid grid-cols-1 gap-2'>
-          <div className='relative'>
+        <div className='inputfield-grid'>
+          <div className='inputfield-position'>
             <label>
-              <span className='sr-only'>Email address</span>
+              <span className='only-screenreaders'>Email address</span>
               <input
                 name='email'
                 type='email'
                 placeholder='jane.doe@example.com'
                 required={true}
-                className='w-full rounded-none border-b border-slate-200 py-3 pl-3 pr-10 text-slate-800 placeholder-slate-400'
+                className='inputfield'
               />
             </label>
-            <button
-              type='submit'
-              className='absolute top-0 right-0 bottom-0 flex items-center justify-center px-3 text-2xl text-slate-400'
-            >
-              <span className='sr-only'>Submit</span>
+            <button type='submit' className='signupform-arrow'>
+              <span className='only-screenreaders'>Submit</span>
               <span aria-hidden={true}>&rarr;</span>
             </button>
           </div>
           {prismicH.isFilled.richText(settings.data.newsletterDisclaimer) && (
-            <p className='text-center text-xs tracking-tight text-slate-500'>
+            <p className='signupform-disclaimer'>
               <PrismicText field={settings.data.newsletterDisclaimer} />
             </p>
           )}
